021a50d85e6a3cec0ed57b719133bff85b88cc6c
Bhikhi <bhikhi19gc@gmail.com>
2021-11-11 15:47:50 +0530

    uploading files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1532,12 +1532,9 @@ $('#myModal').on('shown.bs.modal', function (e) {
 $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
 })
 
-// stop playing the youtube video when I close the modal
-$('#myModal').on('hide.bs.modal', function (e) {
-    // a poor man's stop video
-    $("#video").attr('src',$videoSrc); 
-})  
-  
+$(".video-btn").click( function(e) {
+    e.preventDefault();
+}); 
 // document ready  
 });
 
