39030e9d569edba09a9162b3f0ca4b1754e48dcd
Bhikhi Choudhary <hey.bhikhiGC@gmail.com>
2021-12-02 22:43:14 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1534,6 +1534,12 @@ $('#myModal').on('shown.bs.modal', function (e) {
 $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
 })
 
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',''); 
+})
+	
 // document ready  
 });
 
